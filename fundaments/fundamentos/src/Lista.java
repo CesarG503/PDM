afcba7207988ceadbd7a5f33eeffb8c010824c3d
CesarG503 <cesargaray06@gmail.com>
2026-02-25 00:28:11 -0600

    feat: ArrayList hasmap

--- fundaments/fundamentos/src/Lista.java
+++ fundaments/fundamentos/src/Lista.java
@@ -1,4 +1,7 @@
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Lista
 {
@@ -29,6 +32,46 @@ public class Lista
             System.out.println(numero);
         }
 
+        int index = numeros.indexOf(100);
+
+        if(numeros.contains(100))
+        {
+            System.out.println("si");
+            numeros.remove(index);
+        }
+        for (int item: numeros)
+        {
+            System.out.println(item);
+        }
+
+        HashMap<String, String> diccionario = new HashMap<>(); //como hacer un diccionario
+
+
+        diccionario.put("Hola","como estas");
+
+        diccionario.get("Hola");
+
+
+        Integer array[] = new Integer[5];
+
+        for (int i = 0; i < 5 ; i++)
+        {
+            array[i]=i;
+        }
+
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        lista.addAll(Arrays.asList(array));
+
+        HashMap<Integer, Integer> dic = new HashMap<>();
+
+
+        for (Integer x: lista)
+        {
+            dic.put(x,x);
+        }
+
+        System.out.println(dic);
 
 
 
